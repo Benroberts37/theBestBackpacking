0d4c0e4ad32d1d5d95ffa30d7feece86d77db2bb
Benroberts37 <benrobsemail@gmail.com>
2022-05-11 13:06:59 -0400

    added react router and navbar - all functioning

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import logo from './logo.svg';
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link, } from 'react-router-dom'
+
+import BackpackForm from './Components /backpackForm'
+import Gear from './Components /gear'
+import CommunitySubmissions from './Components /communitySubmissions'
+import Home  from './Components /Home'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          The Best backpacking! Coming soon. 
-        </h1>
-      </header>
-    </div>
+    <Router>
+    <div className = "App"> 
+<>
+  <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">The Best Backpacking</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link as={Link} to='/'>Home</Nav.Link>
+      <Nav.Link as={Link} to='/submitATrip'>Submit a trip</Nav.Link>
+      <Nav.Link as={Link} to='/communitySubmissions'>Community Submissions</Nav.Link>
+      <Nav.Link as={Link} to='/gear'>Gear</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+</>
+<div>
+  <Routes>
+    <Route exact path ='/' element ={<Home/>}></Route>
+    <Route path ='/submitATrip' element ={<BackpackForm/>}></Route>
+    <Route path ='/communitySubmissions' element ={<CommunitySubmissions/>}></Route>
+    <Route path ='/gear' element ={<Gear/>}></Route>
+  </Routes>
+</div>
+</div>
+</Router>
   );
 }
 
---
+++ src/Components /gear.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Gear () {
+    return(
+        <div>
+            <h1>
+                This is the gear page!
+            </h1>
+        </div>
+    )
+}
+
+export default Gear
